4e79ca7e37010b60e15c234344e9071d79871133
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-05-13 16:47:37 +0000

    terminado

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ let noun = ["jogger", "racoon"];
 let domainNames = [];
 
 for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
+  for (let x = 0; x < adj.length; x++) {
     for (let y = 0; y < noun.length; y++) {
-      let domain = pronoun[i] + adj[j] + noun[i] + ".com";
+      let domain = pronoun[i] + adj[x] + noun[i] + ".com";
       domainNames.push(domain);
     }
   }
